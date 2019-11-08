9762bb4e51c42eb9f7c72ee0922e5dc8c03f52f5
Bryan Ordoñez <201404243@ingenieria.usac.edu.gt>
2019-11-08 15:46:17 -0600

    act

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var str = "";
+
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://34.66.43.182/sopes';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+app.route('/mensajes').get(function(req, res)
+
+    {
+            var cursor = db.collection('mensajes').find();
+            cursor.each(function(err, item) {
+
+                if (item != null) {
+                    str = str + "    Employee id  " + item.Employeeid + "</br>";
+                }
+            });
+            res.send(str);
+        
+    });
+
+var server = app.listen(3000, function() {}); 
