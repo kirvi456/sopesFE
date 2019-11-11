ee7a755d17f0ab42a42b72ed08b291898f121829
kirvi456 <kirvi16@gmail.com>
2019-11-11 00:40:20 +0000

    yyyy

--- app.js
+++ app.js
@@ -1,24 +1,62 @@
+
 var express = require('express');
 var app = express();
-var str = "";
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://34.66.43.182:27017/sopes';
+var mongoDB = 'mongodb://34.68.30.68:27017/sopes';
 mongoose.connect(mongoDB,{ useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.listen(3000, function() {}); 
 
-app.route('/mensajes').get(function(req, res)
 
-    {
-        var cursor = db.collection('mensajes').find().toArray(function(err, result){
-            if (err) throw err;
-            return resolve(result);
-        });          	
-        res.send(cursor);        
+app.route('/buscarUsuario').get(function(req, res){
+	var usuarioABuscar = req.body.usuario;
+        var cursor = db.collection('mensajes').find({usuario: usuarioABuscar }).limit(3).toArray(function(err, resultado){
+                if(err) throw res.status(400).send("No se pudo conectar a la base de datos.");;
+                res.json(resultado);
+        });             
     });
 
-var server = app.listen(3000, function() {}); 
+app.route('/buscarCategoria').get(function(req, res){
+        var categoriaABuscar = req.body.categoria;
+        var cursor = db.collection('mensajes').find({categoria: categoriaABuscar }).limit(3).toArray(function(err, resultado){
+                if(err) throw res.status(400).send("No se pudo conectar a la base de datos.");;
+                res.json(resultado);
+        });             
+    });
+
+app.route('/mensajes').get(function(req, res){
+        var cursor = db.collection('mensajes').find().sort({datetime : -1}).toArray(function(err, resultado){
+		if(err) throw res.status(400).send("unable to save to database");;
+		res.json(resultado);
+
+	});          	
+    });
+
+app.route('/mensajes').post(function(req, res)
+    {       
+	var usuario = req.query.usuario;
+	var nombre = req.query.nombre;
+	var txt = req.query.txt;
+	var categoriaAux = txt.match(/#\w*/);
+	var categoria = categoriaAux.toString().replace("#","");
+	var datetime = new Date();
+	var str = "{" +
+			'"usuario": "' + usuario + '",' +
+			'"nombre": "' + nombre + '",' +
+			'"txt": "' + txt + '",' +
+			'"categoria": "' + categoria + '",' +
+			'"datetime": "' + datetime + '"' +
+		'}';
+	var jsonAux = JSON.parse(str);       
+	db.collection("mensajes").insert(jsonAux);	    
+       	res.send("respuesta");
+	});
+
+ 
